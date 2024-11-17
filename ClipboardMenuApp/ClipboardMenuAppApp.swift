24d37c9cdcbca6fd764763231d3c68e3bc69638b
Shariar Khan <shuvho.91@gmail.com>
2024-11-17 08:57:47 +0600

    Initial Commit

---
+++ ClipboardMenuApp/ClipboardMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardMenuAppApp.swift
+//  ClipboardMenuApp
+//
+//  Created by Shahriar Khan on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardMenuApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClipboardMenuApp
+//
+//  Created by Shahriar Khan on 11/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
