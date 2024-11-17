4bc2693171a8fc4f332dfbc31f0f34f88c451e33
Shariar Khan <shuvho.91@gmail.com>
2024-11-17 12:44:45 +0600

    added right click action. right mouse click will show the exit option

--- ClipboardMenuApp/AppDelegate.swift
+++ ClipboardMenuApp/AppDelegate.swift
@@ -6,10 +6,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var popover: NSPopover!
     var clipboardManager = ClipboardManager()
     var eventMonitor: Any?
-
+    var statusMenu: NSMenu!
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Initialize the popover
+        // Create the popover
         popover = NSPopover()
         popover.contentSize = NSSize(width: 300, height: 600)
         popover.behavior = .transient
@@ -19,27 +19,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         if let statusBarButton = statusItem.button {
             statusBarButton.image = NSImage(systemSymbolName: "doc.on.clipboard", accessibilityDescription: "Clipboard")
-            statusBarButton.action = #selector(togglePopover(_:))
+            statusBarButton.action = #selector(statusBarButtonClicked(_:))
+            statusBarButton.target = self
+            statusBarButton.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
-        
+
         // Hide the Dock icon
         NSApp.setActivationPolicy(.accessory)
-        
-        // Add event monitor to close popover when clicking outside
+
+        // Add local event monitor to close popover when clicking outside
         eventMonitor = NSEvent.addLocalMonitorForEvents(matching: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
             if let popover = self?.popover, popover.isShown {
                 popover.performClose(event)
             }
             return event
         }
-        
-        if let eventMonitor = eventMonitor {
-            NSEvent.removeMonitor(eventMonitor)
-        }
 
+        // Create the status menu
+        statusMenu = NSMenu()
 
+        // Create the "Exit" menu item
+        let exitMenuItem = NSMenuItem(title: "Exit", action: #selector(exitApp), keyEquivalent: "q")
+        exitMenuItem.target = self
+        statusMenu.addItem(exitMenuItem)
     }
-    
+
+    @objc func statusBarButtonClicked(_ sender: NSStatusBarButton) {
+        let event = NSApp.currentEvent!
+        if event.type == .rightMouseUp {
+            // Show the menu on right-click
+            statusItem.popUpMenu(statusMenu)
+        } else {
+            // Toggle the popover on left-click
+            togglePopover(sender)
+        }
+    }
+
     @objc func togglePopover(_ sender: AnyObject?) {
         if let statusBarButton = statusItem.button {
             if popover.isShown {
@@ -50,14 +65,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
 
+    @objc func exitApp() {
+        NSApp.terminate(nil)
+    }
 
-//    func applicationWillTerminate(_ notification: Notification) {
-//        if let contentViewController = popover.contentViewController as? NSHostingController<ContentView> {
-//            contentViewController.rootView.clipboardManager.stopMonitoring()
-//        }
-//    }
-
-//    func applicationWillTerminate(_ notification: Notification) {
-//            clipboardManager.stopMonitoring()
-//        }
+    func applicationWillTerminate(_ notification: Notification) {
+        clipboardManager.stopMonitoring()
+        if let eventMonitor = eventMonitor {
+            NSEvent.removeMonitor(eventMonitor)
+        }
+    }
 }
